14268228c3676dfd772c16191fa6c0bdac6d003a
Len Woodward <Len@ProjektGopher.com>
2024-07-12 18:23:58 -0700

    Adds Support\Nothing::class

---
+++ src/Support/Nothing.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ArtisanBuild\CommunityPrompts\Support;
+
+/**
+ * Nothing.
+ * 
+ * `null` is a valid return value, so we use `Nothing`
+ * to indicate that a given loop should continue.
+ * 
+ * @see https://github.com/laravel/prompts/pull/154
+ */
+class Nothing
+{
+    //
+}
