0f2fe3ec7b6812541ea7ac88843b40166e431345
Len Woodward <Len@ProjektGopher.com>
2024-07-12 19:01:22 -0700

    Adds Output/AsyncConsoleOutput::class

---
+++ src/Output/AsyncConsoleOutput.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Laravel\Prompts\Output;
+
+use React\Stream\WritableResourceStream;
+
+class AsyncConsoleOutput extends ConsoleOutput
+{
+    protected WritableResourceStream $stdout;
+
+    /**
+     * Write to the output buffer.
+     */
+    protected function doWrite(string $message, bool $newline): void
+    {
+        $this->stdout ??= new WritableResourceStream(STDOUT);
+        $this->stdout->write($message);
+
+        if ($newline) {
+            $this->stdout->write(PHP_EOL);
+        }
+    }
+
+    /**
+     * Write output directly, bypassing newline capture.
+     */
+    public function writeDirectly(string $message): void
+    {
+        $this->doWrite($message, false);
+    }
+}
