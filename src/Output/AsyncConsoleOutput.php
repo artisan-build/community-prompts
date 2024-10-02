01cb1d0d85efc491569133b5f4d844a45c1a389a
Len Woodward <len@projektgopher.com>
2024-10-01 23:01:52 -0700

    [ Task ] Remove Patched Prompt (#8)
    * move laravel/prompts to require-dev, suggest install, bump version
    
    * require illuminate/collections
    
    * require react/event-loop
    
    * sort composer packages
    
    * Add Override attirbutes to AsyncConsoleOutput
    
    * remove PatchedPrompt, extend and Override Prompt class
    
    * track trailing newlines in AsyncConsoleOutput
    
    * Fix code styling
    
    * drop php 8.1 support
    
    * add warning to docs
    
    ---------
    
    Co-authored-by: ProjektGopher <ProjektGopher@users.noreply.github.com>

--- src/Output/AsyncConsoleOutput.php
+++ src/Output/AsyncConsoleOutput.php
@@ -3,6 +3,7 @@
 namespace ArtisanBuild\CommunityPrompts\Output;
 
 use Laravel\Prompts\Output\ConsoleOutput;
+use Override;
 use React\Stream\WritableResourceStream;
 
 class AsyncConsoleOutput extends ConsoleOutput
@@ -12,19 +13,29 @@ class AsyncConsoleOutput extends ConsoleOutput
     /**
      * Write to the output buffer.
      */
+    #[Override]
     protected function doWrite(string $message, bool $newline): void
     {
+        if ($newline) {
+            $message .= PHP_EOL;
+        }
+
         $this->stdout ??= new WritableResourceStream(STDOUT);
         $this->stdout->write($message);
 
-        if ($newline) {
-            $this->stdout->write(PHP_EOL);
+        $trailingNewLines = strlen($message) - strlen(rtrim($message, PHP_EOL));
+
+        if (trim($message) === '') {
+            $this->newLinesWritten += $trailingNewLines;
+        } else {
+            $this->newLinesWritten = $trailingNewLines;
         }
     }
 
     /**
      * Write output directly, bypassing newline capture.
      */
+    #[Override]
     public function writeDirectly(string $message): void
     {
         $this->doWrite($message, false);
